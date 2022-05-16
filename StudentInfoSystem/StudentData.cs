62ff3dd02e553d6c1d4ee4d21aea392be835bdc9
Ivelin Stoyanov <36234554+IvelinStoyanovIS@users.noreply.github.com>
2022-05-16 18:04:49 +0300

    Add files via upload
    PS_Exc6_IS

---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserLogin;
+
+namespace StudentInfoSystem.TestData
+
+{
+    static public class StudentData
+    {
+
+        static public List<Student> TestStudents { 
+            get
+            {
+                ResetTestStudentData();
+                return _testStudents;
+            } 
+            set { }
+        }
+        
+        static private List<Student> _testStudents;
+
+        static private void ResetTestStudentData()
+        {
+            if (_testStudents == null)
+            {
+                _testStudents = new List<Student>
+                {
+                    new Student
+                    {
+                        Name = "Ivelin",
+                        SecondName = "Svetlozarov",
+                        LastName = "Stoyanov",
+                        Faculty = "FCST",
+                        Speciality = "CSE",
+                        EducationalQualificationDegree = "Bachelor",
+                        StudentStatus = "Редовен",
+                        FacultyNumber = "121219032",
+                        Course = 3,
+                        Stream = 9,
+                        Group = 31,
+                    },
+                    new Student
+                    {
+                        Name = "Petur",
+                        SecondName = "Ivanov",
+                        LastName = "Ivanov",
+                        Faculty = "FCST",
+                        Speciality = "CSE",
+                        EducationalQualificationDegree = "Bachelor",
+                        StudentStatus = "Редовен",
+                        FacultyNumber = "121211032",
+                        Course = 3,
+                        Stream = 9,
+                        Group = 31,
+                    },
+                    new Student
+                    {   Name = "Ivanka",
+                        SecondName = "Petkova",
+                        LastName = "Karaivanova",
+                        Faculty = "FCST",
+                        Speciality = "CSE",
+                        EducationalQualificationDegree = "Bachelor",
+                        StudentStatus = "Редовен",
+                        FacultyNumber = "121222032",
+                        Course = 3,
+                        Stream = 9,
+                        Group = 31,
+                    },
+                };
+            }
+        }
+    }
+}
+
---
+++ UserLogin/UserData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UserLogin
+{
+    public static class UserData
+    {
+       // private static List<User> testUsers;
+
+        public static List<User> TestUsers
+        {
+            get
+            {
+                ResetTestUserData();
+                return _testUsers;
+            }
+            set { }
+        }
+        static private List<User> _testUsers;
+        static private void ResetTestUserData()
+        {
+            if (_testUsers == null)
+            {
+                _testUsers = new List<User>
+                {
+                    new User
+                    {
+                        Username = "ivADo",
+                        Password = "someStrongP@ss",
+                        FacultyNumber = "121219032",
+                        UserRole = UserRoles.ADMIN,
+                        Created = DateTime.Now,
+                        ActiveTo = DateTime.MaxValue
+                    },
+                    new User
+                    {
+                        Username = "ivUSo",
+                        Password = "someStrongP@ss",
+                        FacultyNumber = "121219032",
+                        UserRole = UserRoles.STUDENT,
+                        Created = DateTime.Now,
+                        ActiveTo = DateTime.MaxValue
+                    },
+                    new User
+                    {
+                        Username = "ivUS2o",
+                        Password = "someStrongP@ss",
+                        FacultyNumber = "121219032",
+                        UserRole = UserRoles.STUDENT,
+                        Created = DateTime.Now,
+                        ActiveTo = DateTime.MaxValue
+                    },
+                };
+            }
+        }
+      
+
+        public static User IsUserPassCorrect(string UserName, string PassWord)
+        {
+
+            UserContext context = new UserContext();
+            User user = (from u in context.Users
+                         where u.Username.Equals(UserName) &&
+                         u.Password.Equals(PassWord)
+                         select u).DefaultIfEmpty(null).First();
+
+            return user;
+
+        }
+
+        public static void SetUserActiveTo(string UserName, DateTime date)
+        {
+            foreach (User user in TestUsers)
+            {
+                if (user.Username == UserName)
+                {
+                    user.ActiveTo = date;
+                    Logger.LogActivity("The activity has been changed for " + UserName);
+                }
+            }
+        }
+
+        public static void AssignUserRole(string UserName, UserRoles userRole)
+        {
+            UserContext context = new UserContext();
+
+
+            User usr =
+           (from u in context.Users
+            where u.Username.Equals(UserName)
+            select u).First();
+
+            usr.UserRole = (UserRoles)(int)userRole;
+            Logger.LogActivity("Role has been changed for " + UserName);
+
+            context.SaveChanges();
+
+        }
+        static public void GetUsers()
+        {
+            foreach (User user in TestUsers)
+            {
+                Console.WriteLine("Username: " + user.Username);
+                Console.WriteLine("User password: " + user.Password);
+                Console.WriteLine("User faculty number: " + user.FacultyNumber);
+                Console.WriteLine("User Role: " + (UserRoles)user.UserRole);
+                Console.WriteLine("Created on: " + user.Created);
+                Console.WriteLine("Expire on: " + user.ActiveTo);
+            }
+        }
+
+    }
+}
+
+
+
+    
